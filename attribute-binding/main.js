b14f9142173896eb7f2562866b2c5a54b74497bf
Fabian <murillo.a.fabian@gmail.com>
2019-01-05 22:36:40 -0500

    Connecting data to HTML attributes

---
+++ attribute-binding/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app', // Reference el
+    data: { 
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of Socks",
+        link: "https://www.vuemastery.com/"
+
+
+    }
+})
